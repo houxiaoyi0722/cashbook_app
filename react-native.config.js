e85dc6adc6bd52089874b39630b8d0748ff616c6
houxiaoyi <xiaoyihou@outlook.com>
2026-03-13 16:41:14 +0800

    rneui升级

--- react-native.config.js
+++ react-native.config.js
@@ -7,5 +7,14 @@ module.exports = {
       },
     },
   },
-  assets: ['./node_modules/react-native-vector-icons/fonts'],
-}; 
+  assets: [
+    './node_modules/@react-native-vector-icons/material-icons/fonts',
+    './node_modules/@react-native-vector-icons/material-design-icons/fonts',
+    './node_modules/@react-native-vector-icons/ionicons/fonts',
+    './node_modules/@react-native-vector-icons/fontawesome/fonts',
+    './node_modules/@react-native-vector-icons/feather/fonts',
+    './node_modules/@react-native-vector-icons/entypo/fonts',
+    './node_modules/@react-native-vector-icons/simple-line-icons/fonts',
+    './node_modules/@react-native-vector-icons/ant-design/fonts',
+  ],
+};
