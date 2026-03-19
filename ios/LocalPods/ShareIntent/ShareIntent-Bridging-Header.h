662e6587b74ed331e656abd87af7e5ed33bf90b7
houxiaoyi <xiaoyihou@outlook.com>
2026-03-19 14:55:25 +0800

    action2

---
+++ ios/LocalPods/ShareIntent/ShareIntent-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ShareIntent-Bridging-Header.h
+//  ShareIntent
+//
+
+#ifndef ShareIntent_Bridging_Header_h
+#define ShareIntent_Bridging_Header_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+#endif /* ShareIntent_Bridging_Header_h */
--- ios/LocalPods/ShareIntent/ShareIntent.podspec
+++ ios/LocalPods/ShareIntent/ShareIntent.podspec
@@ -9,7 +9,5 @@ Pod::Spec.new do |s|
   s.platform     = :ios, "13.0"
   s.source       = { :path => "." }
   s.source_files = "*.{h,m,swift}"
-  s.public_header_files = "*.h"
-  s.module_map = "module.modulemap"
   s.dependency "React-Core"
 end
